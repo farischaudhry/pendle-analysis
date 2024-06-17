623163c54f1701500d50cd9762a4a75d6d0757d1
Faris Chaudhry <faris.chaudhry@outlook.com>
2024-06-17 10:48:46 +0100

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('whitegrid')
+
+def prepare_token_data(underlying_path, pt_path, yt_path, start_date, expiry_date):
+    # Load the data
+    pt_df = pd.read_csv(pt_path)
+    yt_df = pd.read_csv(yt_path)
+    underlying_df = pd.read_csv(underlying_path)
+
+    # Convert column names to lowercase
+    pt_df.columns = pt_df.columns.str.lower()
+    yt_df.columns = yt_df.columns.str.lower()
+    underlying_df.columns = underlying_df.columns.str.lower()
+
+    # Convert the timestamp to datetime
+    pt_df['time'] = pd.to_datetime(pt_df['time'])
+    yt_df['time'] = pd.to_datetime(yt_df['time'])
+    underlying_df['date'] = pd.to_datetime(underlying_df['date'])
+
+    pt_df.set_index('time', inplace=True)
+    yt_df.set_index('time', inplace=True)
+    underlying_df.set_index('date', inplace=True)
+
+    # Inner join the dataframes on 'time'
+    prices_df = pt_df[['open']].join(yt_df[['open']], lsuffix='_pt', rsuffix='_yt', how='inner').join(underlying_df[['open']], how='inner')
+
+    # Rename the columns
+    prices_df.columns = ['pt_open', 'yt_open', 'underlying_open']
+
+    # Filter the data by start and expiry dates
+    return prices_df[(prices_df.index >= start_date) & (prices_df.index <= expiry_date)]
+
+def prepare_price_data(path):
+    df = pd.read_csv(path)
+    df.columns = df.columns.str.lower()
+    try:
+        df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y')
+    except ValueError:
+        df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
+    df.set_index('date', inplace=True)
+    return df
